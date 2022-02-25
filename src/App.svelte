edb8ba3615cc5321dc44b5662b714c44f0ac359f
recluze <recluze@gmail.com>
2022-02-25 14:39:08 +0500

    Moved new chit form to component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import TopWaves from "./components/TopWaves.svelte";
 	import Aside from "./components/Aside.svelte";
 	import Header from "./components/Header.svelte";
+	import NewChit from "./components/NewChit.svelte";
 </script>
 
 <div id="app-container" class="app-container">
@@ -11,10 +12,7 @@
 	<section>
 		<div class="container">
 			<main>
-				<div class="chit-entry">
-					<textarea placeholder="Say something..." />
-					<button class="btn-send fa-solid fa-location-arrow" />
-				</div>
+				<NewChit />
 
 				<div class="chit">
 					<div class="author">Nauman</div>
@@ -162,37 +160,5 @@
 		opacity: 0.8;
 		padding-right: 15px;
 		font-size: small;
-	}
-
-	.chit-entry {
-		border: 1px solid #62676d22;
-		display: flex;
-		flex-direction: row;
-		padding: 10px 20px;
-		font-weight: lighter;
-		background-color: #282c34;
-		box-shadow: 0px 0px 20px #1d2025;
-		margin-top: 30px;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	.chit-entry textarea {
-		display: block;
-		flex-basis: 95%;
-		background-color: #282c34;
-		color: #dce4ec;
-		font-size: smaller;
-		padding: 10px;
-		border: 1px solid #dce4ec22;
-	}
-
-	.chit-entry .btn-send {
-		flex-basis: 20px;
-		text-align: right;
-		background-color: inherit;
-		border: none;
-		color: #dce4ec;
-		cursor: pointer;
 	}
 </style>
---
+++ src/components/NewChit.svelte
@@ -0,0 +1,38 @@
+<div class="chit-entry">
+    <textarea placeholder="Say something..." />
+    <button class="btn-send fa-solid fa-location-arrow" />
+</div>
+
+<style>
+    .chit-entry {
+        border: 1px solid #62676d22;
+        display: flex;
+        flex-direction: row;
+        padding: 10px 20px;
+        font-weight: lighter;
+        background-color: #282c34;
+        box-shadow: 0px 0px 20px #1d2025;
+        margin-top: 30px;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .chit-entry textarea {
+        display: block;
+        flex-basis: 95%;
+        background-color: #282c34;
+        color: #dce4ec;
+        font-size: smaller;
+        padding: 10px;
+        border: 1px solid #dce4ec22;
+    }
+
+    .chit-entry .btn-send {
+        flex-basis: 20px;
+        text-align: right;
+        background-color: inherit;
+        border: none;
+        color: #dce4ec;
+        cursor: pointer;
+    }
+</style>
