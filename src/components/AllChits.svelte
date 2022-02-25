8c67a012518aac8dee3fdb062e87984b1acde015
recluze <recluze@gmail.com>
2022-02-25 18:04:38 +0500

    onMount and onDestroy for store

--- src/components/AllChits.svelte
+++ src/components/AllChits.svelte
@@ -1,17 +1,24 @@
 <script>
-    import { ChitStore } from '../stores/ChitStore';
+    import { onMount, onDestroy } from "svelte";
+    import { ChitStore } from "../stores/ChitStore";
     import Chit from "./Chit.svelte";
 
-    let allChits = []
+    let allChits = [];
 
-    ChitStore.subscribe((data) => allChits = data); 
+    let chitStoreUnsub = ChitStore.subscribe((data) => (allChits = data));
+
+    onMount(() => {
+        console.log("Will read data here later... ");
+    });
+
+    onDestroy(() => {
+        console.log("AllChits destroyed ...");
+        chitStoreUnsub();
+    });
 </script>
 
-
 <div class="all-chits">
-
     {#each allChits as chit (chit.id)}
         <Chit {...chit} />
     {/each}
-
 </div>
