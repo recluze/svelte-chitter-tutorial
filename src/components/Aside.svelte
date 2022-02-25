7c9ce23f378116e014766d89805f4da77f3d94f8
recluze <recluze@gmail.com>
2022-02-25 14:22:49 +0500

    Moved extra stuff to components

---
+++ src/components/Aside.svelte
@@ -0,0 +1,21 @@
+<aside>
+    Chitter is a tweet client built on Svelte. Nothing much going on
+    over here. See more on:
+
+    <p>recluze.net/course-svelte</p>
+</aside>
+
+
+
+<style>
+
+	aside {
+		flex-grow: 1;
+		flex-basis: 100px;
+		padding: 20px;
+		font-size: smaller;
+		margin-top: 20px;
+		font-weight: 100;
+		opacity: 0.5;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<!-- Header with information -->
+<nav>
+    <div class="container">
+        <span class="logo"><i class="fa-solid fa-location-arrow" />Chitter</span
+        >
+    </div>
+</nav>
+
+<style>
+    .container {
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+
+    .logo {
+        margin-left: 30px;
+        font-family: "Source Sans Pro";
+        font-weight: 900;
+        opacity: 0.7;
+    }
+
+    .logo i {
+        margin-right: 10px;
+    }
+
+    nav .container {
+        margin-top: 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
